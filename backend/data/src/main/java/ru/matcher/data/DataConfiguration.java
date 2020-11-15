01f9b61d1552cdca62c1534d168eefeb04d409d5
Lixard <Lixard870@gmail.com>
2020-11-15 03:03:18 +0300

    Мелкие исправления
    * Убрал из build'а db_migrations для снижения нагрузки компиляции
    * Добавил @EntityScan аннотацию на DataConfiguration

--- backend/data/src/main/java/ru/matcher/data/DataConfiguration.java
+++ backend/data/src/main/java/ru/matcher/data/DataConfiguration.java
@@ -1,5 +1,6 @@
 package ru.matcher.data;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
  *
  * @author Maxim Borisov
  */
+@EntityScan(basePackages = "ru.matcher.data.model")
 @Configuration
 @ComponentScan
 public class DataConfiguration {
