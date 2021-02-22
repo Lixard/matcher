c74a749df432edef5ab5a15db47031d62927d284
Lixard <Lixard870@gmail.com>
2021-02-22 13:03:49 +0300

    #37 Forbidden 403 error fix

--- backend/security/src/main/java/ru/matcher/security/config/SecurityConfiguration.java
+++ backend/security/src/main/java/ru/matcher/security/config/SecurityConfiguration.java
@@ -66,6 +66,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/users").permitAll()
                 .antMatchers("/auth/**").permitAll()
