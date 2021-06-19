25297e55db4c2e70d823dbd6a4c2ec8c3c42b36e
DarkRacer <schtscherbakov@gmail.com>
2021-06-19 18:28:04 +0300

    Fix mapping

--- backend/security/src/main/java/ru/matcher/security/config/SecurityConfiguration.java
+++ backend/security/src/main/java/ru/matcher/security/config/SecurityConfiguration.java
@@ -72,7 +72,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/auth/**").permitAll()
                 .antMatchers("/swagger-ui/**", "/swagger-resources/**", "/v2/api-docs").permitAll()
                 .antMatchers("/organizations/type/**").permitAll()
-                .antMatchers("/organizations").permitAll()
+                .antMatchers(HttpMethod.POST, "/organizations").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
