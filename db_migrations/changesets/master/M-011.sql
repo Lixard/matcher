714dc4d95934dfa6137741ae6d1b153373b8bf5d
DarkRacer <schtscherbakov@gmail.com>
2021-10-24 15:49:24 +0300

    #74 Added table for request system

---
+++ db_migrations/changesets/master/M-011.sql
@@ -0,0 +1,12 @@
+CREATE TABLE matcher.requests
+(
+    request_id  serial   NOT NULL,
+    project_id  integer,
+    user_id     integer,
+    CONSTRAINT requests_pk PRIMARY KEY (request_id)
+);
+
+ALTER TABLE matcher.requests
+    ADD CONSTRAINT project_fk0 FOREIGN KEY (project_id) REFERENCES matcher.projects (project_id);
+ALTER TABLE matcher.requests
+    ADD CONSTRAINT user_fk1 FOREIGN KEY (user_id) REFERENCES matcher.users (user_id);
