41685616eb69651873ad51490d41a1a0b9771b35
Ilya <ilja.novichikhin@yandex.ru>
2021-11-27 03:33:54 +0300

    #96 Написал скрипт удаления старых тестовых данных

---
+++ db_migrations/changesets/master/M-016.sql
@@ -0,0 +1,4 @@
+DELETE FROM matcher.projects WHERE project_id IN (1, 2, 3);
+DELETE FROM matcher.user_organizations WHERE org_id IN (1, 2, 3, 4);
+DELETE FROM matcher.organizations WHERE org_id IN (1, 2, 3, 4);
+DELETE FROM matcher.users WHERE user_id IN (1, 2, 3, 4);
